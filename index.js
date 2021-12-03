0344f2ef8e9ac1ac5a9b717f3ef09c4b916492e2
RUGMJ7443 <matthewrhysjones@icloud.com>
2021-12-03 17:10:56 +0000

    fixed error handling

--- index.js
+++ index.js
@@ -87,10 +87,11 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({
-			content: 'An error has occurred please try again later.',
-			ephemeral: true,
-		});
+		if (!interaction.replied)
+			await interaction.reply({
+				content: 'An error has occurred please try again later.',
+				ephemeral: true,
+			});
 	}
 });
 
