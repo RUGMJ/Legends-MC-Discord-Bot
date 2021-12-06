ca3b0bbdec87dc380555c65853fe460a0c825344
RUGMJ7443 <matthewrhysjones@icloud.com>
2021-12-06 12:38:23 +0000

    added a ban command

---
+++ commands/ban.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { CommandInteraction } = require('discord.js');
+const multicraftApiNode = require('multicraft-api-node');
+const api = new multicraftApiNode({
+	url: 'https://panel.pebblehost.com/api.php',
+	user: process.env['multicraft-email'],
+	key: process.env['multicraft-api-key'],
+});
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ban')
+		.setDescription(
+			'Allows you to ban a minecraft user from discord, only works for admins.'
+		)
+		.setDefaultPermission(false)
+		.addStringOption(option =>
+			option
+				.setName('user')
+				.setRequired(true)
+				.setDescription('The minecraft user you want to ban.')
+		),
+	/**
+	 *
+	 * @param {CommandInteraction} interaction
+	 */
+	async execute(interaction) {
+		// const result = await api.restartServer({ id: 213276 });
+		const result = await api.sendConsoleCommand({
+			server_id: 213276,
+			command: `ban ${interaction.options.getString('user')}`,
+		});
+		if (!result.success)
+			return await interaction.reply(
+				'Sorry, an error occured with that request'
+			);
+
+		await interaction.reply(
+			`I've *attempted* to ban ${interaction.options.getString('user')}.`
+		);
+	},
+	allowedRoles: ['ADMIN', 'OWNER'],
+};
