c5ada054eae6a99fa90fcee39c128073b2d73e1d
RUGMJ7443 <matthewrhysjones@icloud.com>
2022-03-08 21:40:13 +0000

    made a say command

---
+++ commands/say.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { CommandInteraction } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('say')
+		.setDescription('Says whatever you want :)')
+		.addStringOption(option =>
+			option
+				.setName('message')
+				.setDescription('The message you want to send')
+				.setRequired(true)
+		),
+	/**
+	 *
+	 * @param {CommandInteraction} interaction
+	 */
+	async execute(interaction) {
+		interaction.channel.send(interaction.options.getString('message', true));
+	},
+};
